4db08cec8f5bcfe8262e5b77f11d582b62ae937f
StamChatzis <stanatis977@hotmail.com>
2024-03-22 17:30:50 +0200

    enviroment to environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { firebaseConfig } from 'src/environments/enviroment';
+import { firebaseConfig } from 'src/environments/environment';
 import { LoginButtonComponent } from './shared/components/login-button/login-button.component';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { VideoInitFormComponent } from './components/video-add-form/video-init-form.component';
