33dc0c560c4ce2a062a5cc61854988898f7e91f9
schatzis <schatzis@ots.gr>
2024-05-01 15:54:28 +0300

    added more in profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,27 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject, Observable, tap } from "rxjs";
 import { GmailUser } from "../../models/firestore-schema/user.model";
 import { AuthService } from "../../services/auth.service";
 import { FormControl, FormGroup, Validators} from "@angular/forms";
-import {ProfileService} from "../../services/profile.service";
+import { ProfileService } from "../../services/profile.service";
+import { SupportedLanguages } from "../../models/google/google-supported-languages";
+import { GoogleTranslateService } from "../../services/googletranslate.service";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers: [GoogleTranslateService]
 })
 
 export class ProfileComponent implements OnInit{
   user$: Observable<GmailUser>;
   loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
+  availableLanguages$: BehaviorSubject<SupportedLanguages> = new BehaviorSubject<SupportedLanguages>(null);
   userInfoForm: FormGroup;
+  langSkillsForm: FormGroup;
   editedProfileDetails: boolean = false;
+  langData: any[];
 
-  constructor(private router: Router, public auth: AuthService, public proService: ProfileService) {
+  constructor(private router: Router, public auth: AuthService, public proService: ProfileService, private googleLangService: GoogleTranslateService) {
     this.userInfoForm = new FormGroup({
       displayName: new FormControl( '', [Validators.required, Validators.pattern(/^[A-Za-zΑ-Ωα-ωΆ-Ώά-ώ\s]*$/)]),
-      email: new FormControl( '', [Validators.required, Validators.email])
+      email: new FormControl( '', [Validators.required, Validators.email]),
+      ethnicity: new FormControl(''),
+      bio: new FormControl('')
+    })
+    this.langSkillsForm = new FormGroup({
+      lang: new FormControl(''),
+      skill: new FormControl('')
     })
   }
 
@@ -31,8 +43,9 @@ export class ProfileComponent implements OnInit{
       next: data => {
         this.loadUser(data);
       },
-      error: error => {console.log(error)}
+      error: error => {}
     })
+    this.getAvailableLanguages();
   }
 
   isEdited(){
@@ -42,5 +55,27 @@ export class ProfileComponent implements OnInit{
   loadUser(data: any){
     this.userInfoForm.controls['displayName'].setValue(data.displayName);
     this.userInfoForm.controls['email'].setValue(data.email);
+    this.userInfoForm.controls['bio'].setValue(data.bio);
+    if(data.ethnicity != null){
+      this.userInfoForm.controls['ethnicity'].setValue(data.ethnicity);
+    }else{
+      this.userInfoForm.controls['ethnicity'].setValue('0');
+    }
   }
+
+  getAvailableLanguages(): void {
+    this.googleLangService.getSupportedLanguages()
+      .pipe(tap(() => {
+        this.loading$.next(true)
+      }))
+      .subscribe((response: SupportedLanguages) => {
+        this.availableLanguages$.next(response);
+        this.loading$.next(false)
+      });
+  }
+
+  saveChanges(): void{
+   this.editedProfileDetails = false;
+  }
+
 }
