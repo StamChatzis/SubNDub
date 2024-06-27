becd1c8b1b6fa65cc1dba4f3e183ecb085aa2720
StamChatzis <stanatis977@hotmail.com>
2024-06-27 22:48:52 +0300

    jszip test

--- src/app/services/download-file-handler.service.ts
+++ src/app/services/download-file-handler.service.ts
@@ -66,6 +66,33 @@ export class DownloadFileHandlerService {
     });
   }
 
+  downloadAllSubtitles(userId: string, videoId: string) {
+    const storagePath = `subtitles/${userId}/${videoId}/`;
+    const ref = this.storage.ref(storagePath);
+
+    ref.listAll().subscribe(result => {
+      const subtitles = result.items;
+      const zip = new JSZip();
+
+      subtitles.forEach(subtitle => {
+        subtitle.getDownloadURL().then(url => {
+          fetch(url)
+            .then(response => response.blob())
+            .then(blob => {
+              zip.file(subtitle.name, blob);
+            });
+        });
+      });
+
+      zip.generateAsync({ type: 'blob' }).then(content => {
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(content);
+        link.download = `${videoId}_subtitles.zip`;
+        link.click();
+      });
+    });
+  }
+
 }
 
   // async downloadAllSubtitles(userId: any, videoId: any) {
